3f9c45e24ceec759831220d34ff70420c120357b
Bryan Tamada <btamada@users.noreply.github.com>
2016-06-27 06:00:49 -0700

    Removed api.py

--- api.py
+++
@@ -1,6 +0,0 @@
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import ElementNotVisibleException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
